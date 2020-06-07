5ba71109aec0bde71dec0c27d7cef1135f75510b
Mikiyas Belhu <mikiyasbelhu@gmail.com>
2020-06-07 14:40:29 -0500

    initialize spring boot project

---
+++ src/test/java/edu/miu/cs/cs425/project/miututoring/api/MiuTutoringTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs425.project.miututoring.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiuTutoringTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
