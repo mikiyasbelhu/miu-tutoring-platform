af4e835fd20da4431c760c4803c8587f337ca321
kirubelandargie <kandargie@miu.edu>
2020-06-08 16:23:00 -0500

    add enrollment controller

---
+++ src/main/java/edu/miu/cs/cs425/project/miututoring/api/controller/EnrollmentController.java
@@ -0,0 +1,41 @@
+/*
+package edu.miu.cs.cs425.project.miututoring.api.controller;
+
+import edu.miu.cs.cs425.project.miututoring.api.model.Enrollment;
+import edu.miu.cs.cs425.project.miututoring.api.service.EnrollmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/api/v1/enrollment")
+public class EnrollmentController {
+    EnrollmentService enrollmentService;
+    @Autowired
+    public EnrollmentController(EnrollmentService enrollmentService) {
+        this.enrollmentService = enrollmentService;
+    }
+    @GetMapping(value="/list")
+    public List<Enrollment>listOfEnrollment(){
+        return enrollmentService.getAllEnrollment();
+    }
+    @GetMapping(value="/get/{enrollmentId}")
+    public Enrollment getEnrollment(@PathVariable Integer enrollmentId){
+        return enrollmentService.getEnrollmentById(enrollmentId);
+    }
+    @PutMapping(value="/edit/{enrollmentId")
+    public Enrollment updateEnrollment(@Valid @RequestBody Enrollment enrollment,@PathVariable Integer enrollmentId){
+        return enrollmentService.updateEnrollment(enrollment,enrollmentId);
+    }
+    @PostMapping(value="/register")
+    public Enrollment registerEnrollment(@Valid @RequestBody Enrollment enrollment){
+        return enrollmentService.registerEnrollment(enrollment);
+    }
+    @DeleteMapping(value="/delete")
+    public void deleteEnrollment(@PathVariable Integer enrollmentId){
+        enrollmentService.deleteEnrollmentById(enrollmentId);
+    }
+}
+*/
