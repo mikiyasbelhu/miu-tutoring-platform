fa43816cce693c991d485fa0ed1ee2555c936808
kirubelandargie <kandargie@miu.edu>
2020-06-08 16:22:34 -0500

    add enrollment service

---
+++ src/main/java/edu/miu/cs/cs425/project/miututoring/api/service/EnrollmentService.java
@@ -0,0 +1,15 @@
+package edu.miu.cs.cs425.project.miututoring.api.service;
+
+import edu.miu.cs.cs425.project.miututoring.api.model.Enrollment;
+import edu.miu.cs.cs425.project.miututoring.api.model.TutorialGroup;
+
+
+import java.util.List;
+
+public interface EnrollmentService {
+    List<Enrollment>getAllEnrollment();
+    Enrollment getEnrollmentById(Integer id);
+    Enrollment updateEnrollment(Enrollment updatedEnrollment, Integer enrollmentId);
+    void deleteEnrollmentById(Integer enrollmentId);
+    Enrollment registerEnrollment(Enrollment enrollment);
+}
