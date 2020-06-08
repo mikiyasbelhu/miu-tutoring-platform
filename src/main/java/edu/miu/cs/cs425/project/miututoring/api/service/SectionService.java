249d8bed59c070de8c04c50ebb25182656d9581b
kirubelandargie <kandargie@miu.edu>
2020-06-08 16:20:36 -0500

    add section service

---
+++ src/main/java/edu/miu/cs/cs425/project/miututoring/api/service/SectionService.java
@@ -0,0 +1,14 @@
+package edu.miu.cs.cs425.project.miututoring.api.service;
+
+import edu.miu.cs.cs425.project.miututoring.api.model.Enrollment;
+import edu.miu.cs.cs425.project.miututoring.api.model.Section;
+
+import java.util.List;
+
+public interface SectionService {
+    List<Section> getAllSections();
+    Section getSectionById(Integer sectionId);
+    Section updateSection(Section updatedSection, Integer enrollmentId);
+    void deleteSectionById(Integer sectionId);
+    Section registerSection(Section section);
+}
