4ab18f26e21258bf0f0609a978ab9de953a9c400
Mikiyas Belhu <mikiyasbelhu@gmail.com>
2020-06-14 13:54:08 -0500

    bug fix

--- src/main/java/edu/miu/cs/cs425/project/miututoring/api/service/MyUserDetailsService.java
+++ src/main/java/edu/miu/cs/cs425/project/miututoring/api/service/MyUserDetailsService.java
@@ -5,7 +5,7 @@ import edu.miu.cs.cs425.project.miututoring.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails. UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
