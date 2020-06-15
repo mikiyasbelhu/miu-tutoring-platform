8d0a7c79334175c4570e15ece6037759935fb7de
negestesh <nteshome@miu.edu>
2020-06-14 21:48:00 -0500

    modify data initializer to initialize additional classes

--- src/main/java/edu/miu/cs/cs425/project/miututoring/api/DataInitializer.java
+++ src/main/java/edu/miu/cs/cs425/project/miututoring/api/DataInitializer.java
@@ -24,12 +24,14 @@ public class DataInitializer implements CommandLineRunner {
     TutorialGroupRepository tutorialGroupRepository;
     EnrollmentService enrollmentService;
     PasswordEncoder passwordEncoder;
+    ReportService reportService;
+    TutorRequestService tutorRequestService;
 
     @Autowired
     public DataInitializer(FacultyService facultyService, StudentService studentService,
                            MyUserDetailsService myUserDetailsService, CourseService courseService,
                            SectionService sectionService, TutorialGroupRepository tutorialGroupRepository,
-                           PasswordEncoder passwordEncoder, EnrollmentService enrollmentService){
+                           PasswordEncoder passwordEncoder, EnrollmentService enrollmentService, ReportService reportService, TutorRequestService tutorRequestService) {
         this.studentService = studentService;
         this.facultyService = facultyService;
         this.userService = myUserDetailsService;
@@ -38,6 +40,8 @@ public class DataInitializer implements CommandLineRunner {
         this.tutorialGroupRepository = tutorialGroupRepository;
         this.passwordEncoder = passwordEncoder;
         this.enrollmentService = enrollmentService;
+        this.reportService = reportService;
+        this.tutorRequestService = tutorRequestService;
     }
 
     @Override
@@ -45,74 +49,113 @@ public class DataInitializer implements CommandLineRunner {
 
         // Admin
         List<User> usersList = new ArrayList<User>(Arrays.asList(
-                new User("admin@miu.edu","admin","George","","Cannon", new ArrayList<>(Arrays.asList("ROLE_ADMIN","ROLE_FACULTY")))
+                new User("admin@miu.edu", "admin", "George", "", "Cannon", new ArrayList<>(Arrays.asList("ROLE_ADMIN", "ROLE_FACULTY")))
         ));
         saveUsers(usersList);
 
         // Student
-        Student student1 = new Student("student@miu.edu","student","000-61-0001", "Tesfaye", "Lemma", "Girum", 3.45, LocalDate.of(2019,5,24));
-        Student student2 = new Student("tutor@miu.edu","tutor123","000-61-0002", "Haile", "Teshale", "Alemu", 3.24, LocalDate.of(2019,5,21));
-        Student student3 = new Student("rmhbcd@hi2.in","student","000-61-0003", "Tigist", "Gutema", "Kefyalew", 3.84, LocalDate.of(2019,5,21));
+        Student student1 = new Student("student@miu.edu", "student", "000-61-0001", "Tesfaye", "Lemma", "Girum", 3.45, LocalDate.of(2019, 5, 24));
+        Student student2 = new Student("tutor@miu.edu", "tutor123", "000-61-0002", "Haile", "Teshale", "Alemu", 3.24, LocalDate.of(2019, 5, 21));
+        Student student3 = new Student("rmhbcd@hi2.in", "student", "000-61-0003", "Tigist", "Gutema", "Kefyalew", 3.84, LocalDate.of(2019, 5, 21));
         saveStudent(student1);
         saveStudent(student2);
         saveStudent(student3);
 
         // Faculty
-        Faculty faculty1 = new Faculty("faculty@miu.edu","faculty", "Obinna", "A", "Kalu","Software Engineeing CS425");
+        Faculty faculty1 = new Faculty("faculty1@miu.edu", "faculty", "Obinna", "A", "Kalu", "Software Engineering CS425");
+        Faculty faculty2 = new Faculty("faculty2@miu.edu", "faculty1", "Michael", "", "Zijlstra", "Modern Programming Practices CS401");
         saveFaculty(faculty1);
+        saveFaculty(faculty2);
 
-        // Course        
-        Course course = new Course("CS425", "Software Engineering", 4);
-        saveCourse(course);
+        // Course
+        Course course1 = new Course("CS425", "Software Engineering", 4);
+        Course course2 = new Course("CS401", "Modern Programming Practices", 4);
+        saveCourse(course1);
+        saveCourse(course2);
 
         // Section
-        Section section = new Section("CS425-2020-06-01", "Library 109", "2020-06", course, faculty1);
-        saveSection(section);
+        Section section1 = new Section("CS425-2020-06-01", "Library 109", "2020-06", course1, faculty1);
+        Section section2 = new Section("CS420-2020-06-01", "Library 110", "2020-06", course2, faculty2);
+        saveSection(section1);
+        saveSection(section2);
 
         // Tutorial Group
-        TutorialGroup tutorialGroup1 = new TutorialGroup("CS425",section);
-        TutorialGroup tutorialGroup2 = new TutorialGroup("CS575",section);
+        TutorialGroup tutorialGroup1 = new TutorialGroup("CS425", section1);
+        TutorialGroup tutorialGroup2 = new TutorialGroup("CS425", section1);
+        TutorialGroup tutorialGroup3 = new TutorialGroup("CS401", section2);
+        TutorialGroup tutorialGroup4 = new TutorialGroup("CS401", section2);
         saveTutorialGroup(tutorialGroup1);
         saveTutorialGroup(tutorialGroup2);
+        saveTutorialGroup(tutorialGroup3);
+        saveTutorialGroup(tutorialGroup4);
+
 
         // Enrollment
-        Enrollment enrollment = new Enrollment(student1, section);
+        Enrollment enrollment = new Enrollment(student1, section1);
+        Enrollment enrollment1 = new Enrollment(student2, section2);
         enrollment.setTutorialGroup(tutorialGroup1);
+        enrollment1.setTutorialGroup(tutorialGroup3);
         saveEnrollment(enrollment);
+        saveEnrollment(enrollment1);
 
         List<Enrollment> list = new ArrayList<>();
         list.add(enrollment);
         tutorialGroup1.setEnrollments(list);
         saveTutorialGroup(tutorialGroup1);
 
+        List<Enrollment> list1 = new ArrayList<>();
+        list1.add(enrollment1);
+        tutorialGroup3.setEnrollments(list1);
+        saveTutorialGroup(tutorialGroup3);
+
+        //Tutor Request
+        TutorRequest request1 = new TutorRequest(section1, enrollment, "I have 4 years experience teaching students");
+        saveTutorialRequest(request1);
+        TutorRequest request2 = new TutorRequest(section2,enrollment1, "I have 2 years experience in the tech industry");
+        saveTutorialRequest(request2);
+
+        //Report
+        Report report1 = new Report(student3, tutorialGroup1, course1, "This student is progressing really well");
+        saveReport(report1);
+        Report report2 = new Report(student3, tutorialGroup3, course2, "This student needs to understand inheritance more");
+        saveReport(report2);
+
     }
 
-    public void saveStudent(Student student){
+    public void saveStudent(Student student) {
         studentService.registerStudent(student);
     }
 
-    public void saveFaculty(Faculty faculty){
+    public void saveFaculty(Faculty faculty) {
         facultyService.registerFaculty(faculty);
     }
 
-    public void saveUsers(List<User> users){
+    public void saveUsers(List<User> users) {
         userService.saveUsers(users);
     }
 
-    public void saveCourse(Course course){
+    public void saveCourse(Course course) {
         courseService.saveCourse(course);
     }
 
-    public void saveSection(Section section){
+    public void saveSection(Section section) {
         sectionService.registerSection(section);
     }
 
-    public void saveTutorialGroup(TutorialGroup tutorialGroup){
+    public void saveTutorialGroup(TutorialGroup tutorialGroup) {
         tutorialGroupRepository.save(tutorialGroup);
     }
 
-    public void saveEnrollment(Enrollment enrollment){
+    public void saveEnrollment(Enrollment enrollment) {
         enrollmentService.saveEnrollment(enrollment);
+    }
+
+    public void saveReport(Report report) {
+        reportService.saveReport(report);
+    }
+
+    public void saveTutorialRequest(TutorRequest tutorRequest) {
+        tutorRequestService.saveTutorRequest(tutorRequest);
     }
 
 }
